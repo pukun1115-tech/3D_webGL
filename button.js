37b26ba2ddd93404269139208d4bc52c1f19d993
pukun1115-tech <pukun1115@gmail.com>
2026-09-05 08:26:52 +0900

    ジャンプ

--- button.js
+++ button.js
@@ -4,8 +4,25 @@ breakB.addEventListener("click", () => {
     breakBlock();
 });
 
+const jumpB = document.getElementById("jump");
+
+jumpB.addEventListener("click", () => {
+    const fakeKeyEvent = new KeyboardEvent(
+        "keydown",
+        {
+            key: " ",
+            code: "Space",
+            bubbles: true,
+            cancelable: true
+        }
+    );
+
+    window.dispatchEvent(fakeKeyEvent);
+});
+
 const kisyuB = document.getElementById("kisyuB");
 
 kisyuB.addEventListener("click", () => {
     breakB.classList.toggle("hidden");
+    jumpB.classList.toggle("hidden");
 });
