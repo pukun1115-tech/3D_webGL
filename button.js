1db04d4e9f00a266d0b2628e07886456ac962ebd
pukun1115-tech <pukun1115@gmail.com>
2026-09-05 09:39:03 +0900

    動く

--- button.js
+++ button.js
@@ -6,20 +6,12 @@ const sB = document.getElementById("s");
 const dB = document.getElementById("d");
 const kisyuB = document.getElementById("kisyuB");
 
-const spaceK = {
-    key: " ",
-    code: "Space",
-    bubbles: true,
-    cancelable: true
-};
+const spaceK = { key: " ", code: "Space", bubbles: true, cancelable: true };
 
-const wK = {
-    key: "w",
-    code: "KeyW",
-    bubbles: true,
-    cancelable: true
-};
-
+const wK = { key: "w", code: "KeyW", bubbles: true, cancelable: true };
+const aK = { key: "a", code: "KeyA", bubbles: true, cancelable: true };
+const sK = { key: "s", code: "KeyS", bubbles: true, cancelable: true };
+const dK = { key: "d", code: "KeyD", bubbles: true, cancelable: true };
 
 breakB.addEventListener("click", () => {
     breakBlock();
@@ -36,6 +28,37 @@ jumpB.addEventListener("pointerup", () => {
 
 wB.addEventListener("pointerdown", () => {
     const fakeKeyEvent = new KeyboardEvent("keydown", wK);
+    document.dispatchEvent(fakeKeyEvent);
+});
+wB.addEventListener("pointerup", () => {
+    const fakeKeyEvent = new KeyboardEvent("keyup", wK);
+    document.dispatchEvent(fakeKeyEvent);
+});
+
+aB.addEventListener("pointerdown", () => {
+    const fakeKeyEvent = new KeyboardEvent("keydown", aK);
+    document.dispatchEvent(fakeKeyEvent);
+});
+aB.addEventListener("pointerup", () => {
+    const fakeKeyEvent = new KeyboardEvent("keyup", aK);
+    document.dispatchEvent(fakeKeyEvent);
+});
+
+sB.addEventListener("pointerdown", () => {
+    const fakeKeyEvent = new KeyboardEvent("keydown", sK);
+    document.dispatchEvent(fakeKeyEvent);
+});
+sB.addEventListener("pointerup", () => {
+    const fakeKeyEvent = new KeyboardEvent("keyup", sK);
+    document.dispatchEvent(fakeKeyEvent);
+});
+
+dB.addEventListener("pointerdown", () => {
+    const fakeKeyEvent = new KeyboardEvent("keydown", dK);
+    document.dispatchEvent(fakeKeyEvent);
+});
+dB.addEventListener("pointerup", () => {
+    const fakeKeyEvent = new KeyboardEvent("keyup", dK);
     document.dispatchEvent(fakeKeyEvent);
 });
 
